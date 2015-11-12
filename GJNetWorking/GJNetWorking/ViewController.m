94da9393145f7b9e42882972ce82299cc1469fb9
wangyutao <yutaowang@creditease.cn>
2015-11-12 11:01:47 +0800

    new project

---
+++ GJNetWorking/GJNetWorking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GJNetWorking
+//
+//  Created by wangyutao on 15/11/12.
+//  Copyright © 2015年 wangyutao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
